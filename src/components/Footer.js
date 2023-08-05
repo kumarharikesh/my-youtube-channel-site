d9809bb753087e4f2be2dd5690d7d7f0be678780
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2023-08-05 20:10:45 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,6 @@
 import React from "react";
 import './Footer.css';
 
-// window.smoothScroll() {
-//     var scrollContainer = target;
-//     do {
-//       //find scroll container
-//       scrollContainer = scrollContainer.parentNode;
-//       if (!scrollContainer) return;
-//       scrollContainer.scrollTop += 1;
-//     } while (scrollContainer.scrollTop == 0);
-  
-//     var targetY = 0;
-//     do {
-//       //find the top of target relatively to the container
-//       if (target == scrollContainer) break;
-//       targetY += target.offsetTop;
-//     } while ((target = target.offsetParent));
-  
-//     scroll = function(c, a, b, i) {
-//       i++;
-//       if (i > 30) return;
-//       c.scrollTop = a + ((b - a) / 30) * i;
-//       setTimeout(function() {
-//         scroll(c, a, b, i);
-//       }, 20);
-//     };
-//     // start scrolling
-//     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-//   };
-
-var Scroll = require('react-scroll');
-var scroll = Scroll.animateScroll;
-
-// scroll.scrollToTop();
-
 class App extends React.Component {
   /** @param {Record<string, any>} props */
   constructor(props) {
@@ -42,9 +9,6 @@ class App extends React.Component {
     /*  */ 
     };
   }
-  smoothScroll(){
-    scroll.scrollToTop();
-  };
   render() {
     return (
         <footer className="footer" role="contentinfo" style={{opacity: "1",bottom: "-140px"}}>
